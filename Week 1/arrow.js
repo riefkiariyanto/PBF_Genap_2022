14785bad5590a7095c749224c2c850f41a969417
Muahammad Riefki Ariyanto <70633357+riefkiariyanto@users.noreply.github.com>
2022-02-17 14:47:23 +0700

    Add files via upload

---
+++ Week 1/arrow.js
@@ -0,0 +1,5 @@
+//Menggunakan  arrow
+
+let gretting = message => alert(`${message} Cantik !` );
+
+gretting('Selamat Pagi');
---
+++ Week 1/defaultParameters.js
@@ -0,0 +1,5 @@
+function welcome (user ='Cantik', message = 'Selamat beraktifitas dan jangan lupa makan ya'){
+    alert (`Hai ${user},${message}`);
+}
+
+welcome();

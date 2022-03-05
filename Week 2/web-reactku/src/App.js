3ec26fe880fc4df8b984fbcedce868498a9ff067
riefkiariyanto <riefkiari199@gmail.com>
2022-03-06 02:27:24 +0700

    week 2

---
+++ Week 2/web-reactku/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import AppList from "../src/AppList"
+
+const Appp = ()=>{
+  return (
+    <div>
+    <h1>Hello, World </h1>
+    <AppList/>
+    </div>  
+
+    )
+  }
+
+export default App;
---
+++ Week 2/web-reactku/src/AppList.js
@@ -0,0 +1,14 @@
+import './App.css'
+
+const AppList  = () =>{
+    return(
+        <ul>
+            <li>week 1</li>
+            <li>week 2</li>
+            <li>week 3</li>
+            <li>week 4</li>
+        </ul>
+    )
+}
+
+export default AppList
